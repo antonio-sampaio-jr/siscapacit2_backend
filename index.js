d36821a137ad0f58b35555e11b2be560f0690179
Antonio Sampaio Jr <assoftbel@gmail.com>
2022-12-08 14:32:25 -0300

    OK

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 import express from "express";
 import * as dotenv from "dotenv";
 import dbConnection from "./config/db.config.js";
-import courseRouter from './routes/course.routes.js';
-import govEmployeeRouter from './routes/govemployee.routes.js';
-import coursesfile from './courses.data.json' assert { type: 'json' };
-import govemployeesfile from './govemployee.data.json' assert { type: 'json' };
+import courseRouter from "./routes/course.routes.js";
+import govEmployeeRouter from "./routes/govemployee.routes.js";
+import coursesfile from "./courses.data.json" assert { type: "json" };
+import govemployeesfile from "./govemployee.data.json" assert { type: "json" };
 import CourseModel from "./models/course.model.js";
 import GovEmployeeModel from "./models/govemployee.model.js";
-
 
 dotenv.config();
 
 dbConnection();
+
+await CourseModel.deleteMany();
+await GovEmployeeModel.deleteMany();
 
 await CourseModel.insertMany(coursesfile);
 await GovEmployeeModel.insertMany(govemployeesfile);
@@ -19,11 +21,9 @@ await GovEmployeeModel.insertMany(govemployeesfile);
 const app = express();
 app.use(express.json());
 
-app.use('/cursos', courseRouter);
-app.use('/servidores', govEmployeeRouter);
+app.use("/cursos", courseRouter);
+app.use("/servidores", govEmployeeRouter);
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server up and running on port: ${process.env.PORT}!`);
+  console.log(`Server up and running on port: ${process.env.PORT}!`);
 });
-
-
--- routes/govemployee.routes.js
+++ routes/govemployee.routes.js
@@ -6,90 +6,105 @@ const router = express.Router();
 //API => GOVEMPLOYEE
 
 //1. Cadastrar Servidor (CREATE)
-router.post("/cadastrarServidor",async(request,response)=>{
-    try{
-        const createNewGovEmployee = await GovEmployeeModel.create(request.body);
-        return response.status(201).json(createNewGovEmployee);
-    } catch (error) {
-        console.log(error);
-        return response.status(500).json({msg: "Erro 500 - Falha na Criação do Servidor!"});
-    }
+router.post("/cadastrarServidor", async (request, response) => {
+  try {
+    const createNewGovEmployee = await GovEmployeeModel.create(request.body);
+    return response.status(201).json(createNewGovEmployee);
+  } catch (error) {
+    console.log(error);
+    return response
+      .status(500)
+      .json({ msg: "Erro 500 - Falha na Criação do Servidor!" });
+  }
 });
 
 //2. Listar Servidores (LIST ALL)
-router.get("/listarServidores", async(request,response)=>{
-    try {
-        const getAllGovEmployees = await GovEmployeeModel.find();
-        return response.status(200).json(getAllGovEmployees);
-    } catch (error) {
-        console.log(error);
-        return response.status(500).json({msg: "Erro 500 - Falha na Listagem de Servidores!"});
-    }
-    
+router.get("/listarServidores", async (request, response) => {
+  try {
+    const getAllGovEmployees = await GovEmployeeModel.find();
+    return response.status(200).json(getAllGovEmployees);
+  } catch (error) {
+    console.log(error);
+    return response
+      .status(500)
+      .json({ msg: "Erro 500 - Falha na Listagem de Servidores!" });
+  }
 });
 
 //3. Listar Servidor by ID (LIST)
-router.get("/listarServidor/:id", async(request,response)=>{
-    try {
-        const { id } = request.params;
-        const getGovEmployee = await GovEmployeeModel.findById(id).populate("courses");
-        return response.status(200).json(getGovEmployee);
-    } catch (error) {
-        console.log(error);
-        return response.status(500).json({msg: "Erro 500 - Falha na Listagem do Servidor"});
-    }
-    
+router.get("/listarServidor/:id", async (request, response) => {
+  try {
+    const { id } = request.params;
+    const getGovEmployee = await GovEmployeeModel.findById(id)
+      .populate("inscricoesAbertas")
+      .populate("emAndamento")
+      .populate("concluido");
+    return response.status(200).json(getGovEmployee);
+  } catch (error) {
+    console.log(error);
+    return response
+      .status(500)
+      .json({ msg: "Erro 500 - Falha na Listagem do Servidor" });
+  }
 });
 
 //4. Editar Servidor by ID (UPDATE)
 
-router.put('/editarServidor/:id', async (req, res) => {
-    try {
-        const { id } = req.params
+router.put("/editarServidor/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
 
-        const update = await GovEmployeeModel.findByIdAndUpdate(
-            id,
-            { ...req.body},
-            { new: true, runValidators: true }
-        )
+    const update = await GovEmployeeModel.findByIdAndUpdate(
+      id,
+      { ...req.body },
+      { new: true, runValidators: true }
+    );
 
-        return res.status(200).json(update)
-    } catch (error) {
-        console.log(error)
+    return res.status(200).json(update);
+  } catch (error) {
+    console.log(error);
 
-        return res.status(500).json({ msg: "Erro 500 - Falha na Atualização do Servidor"});
-    }
-})
-
-//5. Excluir Servidor by ID (DELETE) 
-
-router.delete('/excluirServidor/:id', async (req, res) => {
-    try {
-        const { id } = req.params
-
-        const deleteGovEmployee = await GovEmployeeModel.findByIdAndDelete(id)
-
-        return res.status(200).json(deleteGovEmployee);
-
-    } catch (error) {
-        console.log(error)
-
-        return res.status(500).json({ msg: "Erro 500 - Falha na Exclusão do Servidor"})
-    }
-})
-
-//6. Listar Cursos do Servidor
-router.get("/listarCursosServidor/:id", async(request,response)=>{
-    try {
-        const { id } = request.params;
-        const getGovEmployee = await GovEmployeeModel.findById(id).populate("courses");
-        const courses = getGovEmployee.courses;
-        return response.status(200).json(courses);
-    } catch (error) {
-        console.log(error);
-        return response.status(500).json({msg: "Erro 500 - Falha na Listagem do Servidor"});
-    }
-    
+    return res
+      .status(500)
+      .json({ msg: "Erro 500 - Falha na Atualização do Servidor" });
+  }
 });
+
+//5. Excluir Servidor by ID (DELETE)
+
+router.delete("/excluirServidor/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const deleteGovEmployee = await GovEmployeeModel.findByIdAndDelete(id);
+
+    return res.status(200).json(deleteGovEmployee);
+  } catch (error) {
+    console.log(error);
+
+    return res
+      .status(500)
+      .json({ msg: "Erro 500 - Falha na Exclusão do Servidor" });
+  }
+});
+
+// Essa rota me parece desnecessária, porque todas as informações daqui já estão em "3. Listar Servidor by ID (LIST)"
+// mas, se a decisão for por incluí-la, ela precisa ser subdividida em 3: "Inscrições Abertas", "Em Andamento" e "Concluído"
+// //6. Listar Cursos do Servidor
+// router.get("/listarCursosServidor/:id", async (request, response) => {
+//   try {
+//     const { id } = request.params;
+//     const getGovEmployee = await GovEmployeeModel.findById(id).populate(
+//       "courses"
+//     );
+//     const courses = getGovEmployee.courses;
+//     return response.status(200).json(courses);
+//   } catch (error) {
+//     console.log(error);
+//     return response
+//       .status(500)
+//       .json({ msg: "Erro 500 - Falha na Listagem do Servidor" });
+//   }
+// });
 
 export default router;
