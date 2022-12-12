73667b7e2b090fcdf66a36de567766f3c69d956f
Pedro Costa <pedroclc@gmail.com>
2022-12-12 11:20:00 -0300

    12/12/2022 11:20

---
+++ models/autentication.model.js
@@ -0,0 +1,28 @@
+import { model, Schema } from "mongoose";
+
+const autenticationSchema = new Schema({
+  nome: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    match: /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/gm,
+  },
+  senha: {
+    type: String,
+    required: true,
+  },
+  perfil: {
+    type: String,
+    enum: ["usuário", "admin"],
+    default: "usuário",
+  },
+});
+const AutenticationModel = model("Autentication", autenticationSchema);
+
+export default AutenticationModel;
